f5cb0a9dccccbb8e3c3d58eaa5182cbf70f84bd3
angkrnia <anggakurnia712@gmail.com>
2024-10-06 11:49:20 +0700

    upate: auto reset

--- app/Console/Commands/AutoResetAntrian.php
+++ app/Console/Commands/AutoResetAntrian.php
@@ -29,20 +29,19 @@ class AutoResetAntrian extends Command
      */
     public function handle()
     {
-        Log::info("RUNNING - " . date('Y-m-d H:i:s'));
-        // $latestQueue = Queue::latest()->first();
-        // $data = [
-        //     'last_queue' => $latestQueue->queue,
-        //     'status_queue' => $latestQueue->status,
-        //     'created_at' => Carbon::now()->setTimezone('Asia/Jakarta')->toDateTimeString(),
-        // ];
-        // if ($latestQueue->status !== 'waiting' && $latestQueue->status !== 'on waiting') {
-        //     $latestQueue->update(['is_last_queue' => true]);
-        //     $data['status_jobs'] = 'SUCCESS';
-        // } else {
-        //     $data['status_jobs'] = 'FAILED';
-        // }
-        // Log::info($data['status_jobs'] . " - " . date('Y-m-d H:i:s'));
-        // DB::table('jobs_auto_reset_antrian')->insert($data);
+        $latestQueue = Queue::latest()->first();
+        $data = [
+            'last_queue' => $latestQueue->queue,
+            'status_queue' => $latestQueue->status,
+            'created_at' => Carbon::now()->setTimezone('Asia/Jakarta')->toDateTimeString(),
+        ];
+        if ($latestQueue->status !== 'waiting' && $latestQueue->status !== 'on waiting') {
+            $latestQueue->update(['is_last_queue' => true]);
+            $data['status_jobs'] = 'SUCCESS';
+        } else {
+            $data['status_jobs'] = 'FAILED';
+        }
+        Log::info($data['status_jobs'] . " - " . date('Y-m-d H:i:s'));
+        DB::table('jobs_auto_reset_antrian')->insert($data);
     }
 }
